8103419ada27a3e872683003f5d9b005f1477076
Luis Fernando <fernando.chamorro@onetree.com>
2023-07-30 16:53:51 -0500

    Implemented Number pipes (Decimal, Currency, Percent)

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.css']
+  styleUrls: ['./numbers-page.component.css'],
 })
 export class NumbersPageComponent {
-
+  public totalSells = 5848746.544;
+  public percent = 0.5478;
 }
