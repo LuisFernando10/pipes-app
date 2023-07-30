658c60a2dfa04c5b08977a2d9d178a58d769cb62
Luis Fernando <fernando.chamorro@onetree.com>
2023-07-29 21:57:57 -0500

    Implemented lower, upper and title case pipes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
-import { MenuModule } from 'primeng/menu';
+import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
-  exports: [MenuModule, MenubarModule],
+  exports: [
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    PanelModule,
+  ],
 })
 export class PrimeNgModule {}
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
-
+  public lowercase = 'fernando';
+  public uppercase = 'FERNANDO';
+  public titlecase = 'FerNaNDo cHAMorRo';
 }
