e30c9066f0c012673e4370f174c4f9c0ff047bd4
Luis Fernando <fernando.chamorro@onetree.com>
2023-08-22 20:34:00 -0500

    Implemented Table PrimeNG

--- src/app/products/pages/orders-page/orders-page.component.ts
+++ src/app/products/pages/orders-page/orders-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces';
 
 @Component({
   selector: 'app-orders-page',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class OrdersPageComponent {
   public isUpperCase: boolean = true;
+  public heroes: Hero[] = [
+    {
+      canFly: false,
+      color: Color.red,
+      name: 'Flash',
+    },
+    {
+      canFly: true,
+      color: Color.black,
+      name: 'Super Man',
+    },
+    {
+      canFly: false,
+      color: Color.green,
+      name: 'Hulk',
+    },
+    {
+      canFly: true,
+      color: Color.black,
+      name: 'BathMan',
+    },
+    {
+      canFly: false,
+      color: Color.blue,
+      name: 'Robin',
+    },
+  ];
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
